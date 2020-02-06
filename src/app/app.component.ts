f04e1f85907510a7524bdd0cfd4cea00cc2b1958
MGroniu <mikolaj.gronek@gmail.com>
2020-02-06 08:33:30 +0100

    3 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  newTask: string;
-  taskList: Array<string> = [];
-  taskDone: Array<string> = [];
 
-  add() {
-    this.taskList.push(this.newTask);
-    this.newTask = "";
-  }
-
-  remove(task: string) {
-    this.taskList = this.taskList.filter(e => e != task);
-  }
-
-  done(task: string) {
-    this.taskDone.push(task);
-    this.remove(task);
-  }
-
-  clearDone(){
-    this.taskDone = [];
-  }
+  taskList = ['Sprzatanie', 'Gotowanie'];
+  
 }
